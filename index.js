6bb2d0ff9fc5a045776a2080e27cfb3c40d8e53b
Diegoo11 <ynoacamino@gmail.com>
2023-06-16 15:59:21 -0500

    tsting queary and mutations

--- index.js
+++ index.js
@@ -1,30 +1,97 @@
-const { ApolloServer } = require('apollo-server');
+import { gql, ApolloServer, UserInputError } from 'apollo-server';
+import { v1 as uuid } from 'uuid';
 
-const morgan = require('morgan');
+const persons = [
+  {
+    name: 'midu',
+    phone: '044-1234',
+    street: 'calle frontend',
+    city: 'barcelona',
+    id: '123naie12n31oibvo1io231',
+  },
+  {
+    name: 'hola',
+    phone: '012144-123234',
+    street: 'calle frontend',
+    city: 'arquipa',
+    id: '90879213io1io231',
+  },
+  {
+    name: 'xd',
+    phone: '12310144-1234',
+    street: 'calle hola',
+    city: 'tokyo',
+    id: 'oiujb5ujb123',
+  },
+];
 
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+    address: String!
+    direccion: Direccion
+  }
 
-morgan()
+  type Direccion {
+    street: String!
+    city: String!
+  }
 
-
-const typeDefs = `
   type Query {
-    info: String!
+    personCount: Int!
+    allPersons: [Person]!
+    findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
   }
 `;
 
 const resolvers = {
   Query: {
-    info: () => "This is the API of a HAcker"
-  }
-}
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => {
+      const { name } = args;
+      return persons.find((person) => person.name === name);
+    },
+  },
+  Person: {
+    address: (root) => `${root.street}, ${root.city}`,
+    direccion: (root) => ({
+      street: root.street,
+      city: root.city,
+    }),
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find((p) => p.name === args.name)) {
+        throw new UserInputError('The user is alredy registered', {
+          invalidArgs: args.name,
+        });
+      }
+      const person = { ...args, id: uuid() };
+      persons.push(person);
+      return person;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
 server
   .listen()
-  .then(({ url }) => console.log(`Server is running on ${url}`))
-
-  
+  .then(({ url }) => console.log(`Server is running on ${url}`));
