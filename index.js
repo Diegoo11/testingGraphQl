a414944e4e2a303b92d69bcda369f5c286bcff53
Yenaro Joel Noa Camino <97652778+Diegoo11@users.noreply.github.com>
2023-06-16 02:20:55 +0000

    init project

---
+++ index.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require('apollo-server');
+
+const morgan = require('morgan');
+
+
+morgan()
+
+
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    info: () => "This is the API of a HAcker"
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server
+  .listen()
+  .then(({ url }) => console.log(`Server is running on ${url}`))
+
+  
