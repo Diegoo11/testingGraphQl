d325e8fc13ab1c119a771cfefd3199687640077c
Diegoo11 <ynoacamino@gmail.com>
2023-06-21 21:01:52 -0500

    config for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
+  extends: 'airbnb',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
