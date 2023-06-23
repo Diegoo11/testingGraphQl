03d077b48adb8727b8cc6b8a7f06aab237f611d5
Diegoo11 <ynoacamino@gmail.com>
2023-06-23 18:33:41 -0500

    auth in graphQL added and jwt

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable import/extensions */
-import { gql, ApolloServer, UserInputError } from 'apollo-server';
+import {
+  gql, ApolloServer, UserInputError, AuthenticationError,
+} from 'apollo-server';
 import './db.js';
+import jwt from 'jsonwebtoken';
 import Person from './models/person.js';
-/*
-const persons = [
-  {
-    name: 'midu',
-    phone: '044-1234',
-    street: 'calle frontend',
-    city: 'barcelona',
-    id: '123naie12n31oibvo1io231',
-  },
-  {
-    name: 'hola',
-    phone: '012144-123234',
-    street: 'calle frontend',
-    city: 'arquipa',
-    id: '90879213io1io231',
-  },
-  {
-    name: 'xd',
-    phone: '12310144-1234',
-    street: 'calle hola',
-    city: 'tokyo',
-    id: 'oiujb5ujb123',
-  },
-]; */
+import User from './models/User.js';
+
+const SECRET = 'MI_PALABRA_SECRETA_DE_JSONWEBTOKEN';
 
 const typeDefs = gql`
   enum YesNo {
     YES
     NO 
+  }
+
+  type User {
+    username: String!
+    friends: [Person]!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
   }
 
   type Person {
@@ -52,6 +45,7 @@ const typeDefs = gql`
     personCount: Int!
     allPersons(phone: YesNo): [Person]!
     findPerson(name: String!): Person
+    me: User
   }
 
   type Mutation {
@@ -66,6 +60,19 @@ const typeDefs = gql`
       name: String!
       phone: String!
     ): Person
+
+    createUser(
+      username: String!
+    ): User
+
+    loginUser(
+      username: String!
+      password: String!
+    ): Token
+
+    addAsFriend(
+      name: String!
+    ): User
   }
 `;
 
@@ -79,6 +86,7 @@ const resolvers = {
       return Person.find({ phone: { $exists: args.phone === 'YES' } });
     },
     findPerson: async (root, args) => Person.findOne({ name: args.name }),
+    me: (root, args, context) => context.currentUser,
   },
   Person: {
     address: (root) => `${root.street}, ${root.city}`,
@@ -88,13 +96,18 @@ const resolvers = {
     }),
   },
   Mutation: {
-    addPerson: (root, args) => {
+    addPerson: async (root, args, context) => {
+      const { currentUser } = context;
+      if (!currentUser) throw new AuthenticationError('Not Authenticated');
       const person = new Person({ ...args });
       try {
-        return person.save();
+        await person.save();
+        currentUser.friends = currentUser.friends.concat(person);
+        currentUser.save();
       } catch {
         throw new UserInputError('El usuario no se pudo registrar');
       }
+      return person;
     },
     editPhone: async (root, { name, phone }) => {
       const person = await Person.findOne({ name });
@@ -108,15 +121,71 @@ const resolvers = {
         throw new UserInputError('No se pudo guardar al Person');
       }
     },
+    createUser: (root, args) => {
+      const user = new User({ username: args.username });
+      return user.save().catch((error) => {
+        throw new UserInputError(
+          error.message,
+          { invalidArgs: args },
+        );
+      });
+    },
+    loginUser: async (root, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user || password !== 'psw') {
+        throw new AuthenticationError('wrong credentials');
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return {
+        value: jwt.sign(userForToken, SECRET),
+      };
+    },
+    addAsFriend: async (root, args, context) => {
+      const { currentUser } = context;
+      if (!currentUser) throw new AuthenticationError('Not Authenticated');
+
+      const person = await Person.findOne({ name: args.name });
+      const friendVerify = (prs) => !currentUser.friends
+        .map((p) => JSON.stringify(p._id))
+        .includes(JSON.stringify(prs._id));
+      if (!person || !friendVerify(person)) {
+        throw new UserInputError('Usuario no encontrado o ya es un amigo', {
+          invalidArgs: args,
+        });
+      }
+      currentUser.friends = currentUser.friends.concat(person);
+      await currentUser.save();
+      return currentUser;
+    },
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // eslint-disable-next-line consistent-return
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const tk = auth.substring(7);
+      const decodedToken = jwt.decode(tk, SECRET);
+
+      if (!decodedToken) throw new AuthenticationError('Wrong credentials');
+
+      const { id } = decodedToken;
+
+      const currentUser = await User.findById(id).populate('friends');
+      return { currentUser };
+    }
+  },
 });
 
 server
   .listen()
-  // eslint-disable-next-line no-console
   .then(({ url }) => console.log(`Server is running on ${url}`));
